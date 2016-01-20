64ee59fee06e6a9f41ca2a8d8c2b516912096894
Hiroshi Inomata <houkouws@gmail.com>
2016-01-20 10:26:03 +0900

    投稿保存部分（create）の作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,4 +12,14 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.new(create_params)
+    @article.save
+  end
+
+  private
+    def create_params
+      params.require(:article).permit(:name, :title, :text)
+    end
+
 end
