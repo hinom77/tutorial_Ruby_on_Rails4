358ecad6fc5c62f5ff776516740261c9299b5b53
Hiroshi Inomata <houkouws@gmail.com>
2016-05-03 03:35:43 +0900

    ユーザー機能にname項目を追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317120530) do
+ActiveRecord::Schema.define(version: 20160502182136) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160317120530) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "name",                   limit: 255
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
